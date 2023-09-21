8daac824e8eb41aea977c1ba6d56e558efd9d4e0
javohirpython <Javohirpython@gmail.com>
2023-09-21 23:10:17 +0500

    Add GitHub Actions workflow

--- films/views.py
+++ films/views.py
@@ -16,6 +16,8 @@ from rest_framework.authentication import TokenAuthentication
 from .permissions import IsOwner
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from rest_framework import filters
 from rest_framework.pagination import LimitOffsetPagination
 
--- netflix/settings.py
+++ netflix/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
@@ -78,23 +79,23 @@ WSGI_APPLICATION = 'netflix.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres",
-#         "PASSWORD": "123",
-#         "HOST": "127.0.0.1",
-#         "PORT": "5432",
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "netflix",
+        "USER": "odinakhon",
+        "PASSWORD": "odina04",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
